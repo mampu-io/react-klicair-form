c77705e0fd1b2b25a62e84bba689c5d9c4477527
andrian-klikcair <andrian@klikcair.com>
2022-08-23 15:23:28 +0700

    chore(rollup): update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,41 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
 
 const packageJSON = require('./package.json');
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      file: packageJSON.main,
-      format: 'cjs',
-    },
-    {
-      file: packageJSON.module,
-      format: 'esm',
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve({ browser: true }),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss(),
-    terser(),
-  ],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJSON.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJSON.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    external: [/\.scss$/],
+    plugins: [dts()],
+  },
+];
