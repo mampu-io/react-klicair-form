31976a3d1149bd9731f3f1ba13691f90d461f785
andrian-klikcair <andrian@klikcair.com>
2022-08-24 09:37:08 +0700

    fix(entry-point): fix entry point, export both components and type declarations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 // Atoms
 export { default as Button } from './components/atoms/Button';
+export * from './components/atoms/Button';
 export { default as Dropdown } from './components/atoms/Dropdown';
+export * from './components/atoms/Dropdown';
 export { default as InputField } from './components/atoms/InputField';
+export * from './components/atoms/InputField';
 export { default as Label } from './components/atoms/Label';
+export * from './components/atoms/Label';
 export { default as RoundedButton } from './components/atoms/RoundedButton';
+export * from './components/atoms/RoundedButton';
 export { default as TextArea } from './components/atoms/TextArea';
+export * from './components/atoms/TextArea';
 
 // Molecules
 export { default as FormGroup } from './components/molecules/FormGroup';
+export * from './components/molecules/FormGroup';
 
 // Organisms
 export { default as FormWrapper } from './components/organisms/FormWrapper';
+export * from './components/organisms/FormWrapper';
